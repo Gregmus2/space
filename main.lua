20c05c85f287a51af6921aeb08fa32e9d9b54cb6
Konstantin Musorin <gregmus4@gmail.com>
2019-01-05 22:17:47 +0200

    start work on particle system

--- main.lua
+++ main.lua
@@ -7,12 +7,65 @@ local actions = {}
 local mouseMovedActions = {}
 
 function love.load()
+    local img = love.graphics.newImage('particle.jpg')
+    --[[
+        moveTo - двигаться к x, y
+        pause - остановить выбрасывание частиц
+        reset - удалить выпущенные частицы и сбросить таймер жизни
+        setEmissionArea - спавнить только в определенной зоне. args(uniform|normal|ellipse|borderellipse|borderrectangle, отклонение x, отклонение y, угол, directionRelativeToCenter)
+        setBufferSize - кол-во частиц, существующих единовременно
+        setColors( r1, g1, b1, a1, r2, g2, b2, a2, ..., r8, g8, b8, a8 ) - диапазон цветов
+        setDirection(radians) - направление частиц в радианах
+        setEmissionRate(rate) - частиц в сек
+        setEmitterLifetime(sec) - время жизни эмитера
+        setInsertMode(top|bottom|random) - размещение новых частиц на z-оси
+        setLinearAcceleration( xmin, ymin, xmax, ymax ) - скорость частиц (направление вперед, назад)
+        setLinearDamping( min, max ) - замедление
+        setOffset( x, y )
+        setParticleLifetime( min sec, max sec )
+        setPosition( x, y )
+        setQuads( quad1, quad2, ... ) - используется для анимации. квады - это фигуры, которые будет менять частица со временем
+        setRelativeRotation
+        setRotation( min rad, max rad )
+        setSizeVariation
+        setSizes( size1, size2, ..., size8 )
+        setSpeed( min, max )
+        setSpin( min, max )
+        setSpinVariation( variation )
+        setSpread( spread radians )
+        start
+        stop
+    --]]
+    --psystem = love.graphics.newParticleSystem(img, 256)
+    --psystem:setParticleLifetime(2, 5)
+    --psystem:setEmissionArea('normal', 20, 5, 2.30)
+    --psystem:setEmissionRate(50)
+    --psystem:setSizeVariation(0)
+    --psystem:setSpeed(100, 100)
+    --psystem:setDirection(4.18879)
+    --psystem:setSizes(0.5, 0.3, 0.1)
+    --psystem:setLinearAcceleration(-100, -100, -80, -80) -- Random movement in all directions.
+    --psystem:setColors(255, 255, 255, 255, 255, 255, 255, 0) -- Fade to transparency.
+
+    img = love.graphics.newImage('white.jpg')
+    psystem = love.graphics.newParticleSystem(img, 8224)
+    psystem:setParticleLifetime(2, 5)
+    psystem:setEmissionArea('normal', 20, 5, 2.30)
+    psystem:setEmissionRate(1000)
+    psystem:setSizeVariation(0)
+    psystem:setSpeed(300, 300)
+    psystem:setDirection(4.18879)
+    psystem:setSizes(0.5, 0.3, 0.1)
+    psystem:setLinearAcceleration(-100, -100, -80, -80) -- Random movement in all directions.
+    psystem:setColors(255, 0, 0, 255, 255, 122, 0, 255, 255, 255, 0, 255, 255, 255, 0, 0) -- Fade to transparency.
+
     App.load()
 
     App.scene:load()
 end
 
 function love.update(dt)
+    psystem:update(dt)
     App.update(dt)
 
     for _,action in pairs(actions) do
@@ -80,4 +133,6 @@ function love.draw()
         App.scene:draw(App.scene.drawableObjects[i])
     end
     App.scene.menu:draw()
+
+    love.graphics.draw(psystem, love.graphics.getWidth() / 2, love.graphics.getHeight() / 2)
 end
