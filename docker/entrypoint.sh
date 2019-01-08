b040ce50817d3fcff5f6ffa5a2a60948cbaca3e6
Konstantin Musorin <k.musorin@keepsolid.com>
2019-01-08 15:23:39 +0200

    mac build

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,6 +3,15 @@
 zip -9 -r Space.love .
 cat /love-win/love-11.2.0-win64/love.exe Space.love > Space.exe
 mv Space.exe /love-win/dist
-zip -j -r Space.zip /love-win/dist
-chmod 777 -R Space.zip
+zip -j -r Space_win.zip /love-win/dist
+chmod 777 -R Space_win.zip
+
+cp Space.love /love-mac/Space.app/Contents/Resources/
+zip -y -r Space_mac.zip /love-mac/Space.app
+chmod 777 -R Space_mac.zip
+
+#cp Space.love /love-android-sdl2/app/src/main/assets/game.love
+#ant debug
+#cp ~/bin/love_android_sdl2-debug.apk ./
+
 rm Space.love
